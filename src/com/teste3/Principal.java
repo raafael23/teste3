e196c14f3de1b2311a4d1783eab11f2d6f983178
Rafael <rafael@MacBook-Air-de-Rafael-2.local>
2020-06-28 00:51:29 -0300

    hello one more time

--- src/com/teste3/Principal.java
+++ src/com/teste3/Principal.java
@@ -5,5 +5,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("Hello again");
+		System.out.println("Hello one more time");
 	}
 }
