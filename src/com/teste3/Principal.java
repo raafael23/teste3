dbc4d3fbd7930a3406a02759717dbef7f591997e
Rafael <rafael@MacBook-Air-de-Rafael-2.local>
2020-06-28 01:01:56 -0300

    ok

--- src/com/teste3/Principal.java
+++ src/com/teste3/Principal.java
@@ -4,5 +4,11 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+<<<<<<< Updated upstream
+=======
+		System.out.println("Hello again");
+		System.out.println("Hello one more time");
+		
+>>>>>>> Stashed changes
 	}
 }
